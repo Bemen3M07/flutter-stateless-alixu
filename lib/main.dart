db008a1accbf1786774e73ae542175868b6a5dac
Alicia Dionicio <aldiaz24@bemen3.cat>
2025-11-20 20:52:10 +0100

    EJ4 CORRECCION

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,20 @@
 import 'package:flutter/material.dart';
-import 'cat/bemen/dam/0488/ra2/p4/ejercicio8.dart';
 
-void main() => runEjercicio8();
+// Importa tu ejercicio (ajusta la ruta si es necesario)
+import 'cat/bemen/dam/0488/ra2/p4/ejercicio4.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Ejercicio4(), // ← ARRANCA directamente el ejercicio 4
+    );
+  }
+}
